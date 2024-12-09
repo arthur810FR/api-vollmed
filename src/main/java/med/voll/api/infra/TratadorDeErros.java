c93375ffd79a8fbf387f8370ae9808d376870669
Arthur Azevedo <arthurfranciscoazevedo@gmail.com>
2024-12-09 10:02:55 -0300

    Ajeitando o json do erro 400 para vir apenas os campos que estão faltando

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,6 +2,7 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,7 +19,13 @@ public class TratadorDeErros {
     public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
         var erros = ex.getFieldErrors();
 
-        return ResponseEntity.badRequest().body(erros);
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
 
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
